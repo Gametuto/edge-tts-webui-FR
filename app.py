d0cf47354e56f011613ec1642e00e5e518bc30f2
Gametuto <82194297+Gametuto@users.noreply.github.com>
2023-08-18 22:13:51 +0200

    Update app.py
    Translated in French

--- app.py
+++ app.py
@@ -14,13 +14,13 @@ SUPPORTED_VOICES = {
     'shaanxi-Xiaoni-陕西晓妮': 'zh-CN-shaanxi-XiaoniNeural'
 }
 
-# 发音切换
+# Changement de voix
 def changeVoice(voices):
     example = SUPPORTED_VOICES[voices]
     example_file = os.path.join(os.path.dirname(__file__), "example/"+example+".wav")
     return example_file
 
-# 文本转语音
+# Texte en parole
 async def textToSpeech(text, voices, rate, volume):
     output_file = "output.mp3"
     voices = SUPPORTED_VOICES[voices]
@@ -42,11 +42,11 @@ async def textToSpeech(text, voices, rate, volume):
     if (os.path.exists(audio_file)):
         return audio_file
     else:
-        raise gr.Error("转换失败！")
+        raise gr.Error("La conversion a échoué !")
         return FileNotFoundError
 
 
-# 清除转换结果
+# Effacer les résultats de la conversion
 def clearSpeech():
     output_file = os.path.join(os.path.dirname(__file__), "output.mp3")
     if (os.path.exists(output_file)):
@@ -54,15 +54,15 @@ def clearSpeech():
     return None, None
 
 
-with gr.Blocks(css="style.css", title="文本转语音") as demo:
+with gr.Blocks(css="style.css", title="Text to Speech") as demo:
     gr.Markdown("""
-    # 微软Edge文本转语音
-    调用edge-tts 进行转换
+    # Texte en parole avec Microsoft Edge
+    Utilisation de edge-tts pour la conversion
     """)
     with gr.Row():
         with gr.Column():
-            text = gr.TextArea(label="文本", elem_classes="text-area")
-            btn = gr.Button("生成", elem_id="submit-btn")
+            text = gr.TextArea(label="Texte", elem_classes="text-area")
+            btn = gr.Button("Générer", elem_id="submit-btn")
         with gr.Column():
             voices = gr.Dropdown(choices=[
                 "Xiaoxiao-晓晓", "Xiaoyi-晓伊", "Yunjian-云健", "Yunxi-云希",
@@ -70,35 +70,35 @@ with gr.Blocks(css="style.css", title="文本转语音") as demo:
                 "shaanxi-Xiaoni-陕西晓妮"
             ],
                                  value="Xiaoxiao-晓晓",
-                                 label="发音",
-                                 info="请选择发音人",
+                                 label="Voix",
+                                 info="Veuillez choisir une voix",
                                  interactive=True)
             
-            example = gr.Audio(label="试听",
+            example = gr.Audio(label="Exemple",
                               value="example/zh-CN-XiaoxiaoNeural.wav",
                               interactive=False,
-                              elem_classes="example")
+                              elem_classes="exemple")
 
             voices.change(fn=changeVoice,inputs=voices,outputs=example)
             rate = gr.Slider(-100,
                              100,
                              step=1,
                              value=0,
-                             label="语速增减",
-                             info="加快或减慢语速",
+                             label="Augmentation/diminution de la vitesse",
+                             info="Accélérer ou ralentir la vitesse",
                              interactive=True)
             
             volume = gr.Slider(-100,
                                100,
                                step=1,
                                value=0,
-                               label="音调增减",
-                               info="加大或减小音调",
+                               label="Augmentation/diminution du volume",
+                               info="Augmenter ou diminuer le volume",
                                interactive=True)
-            audio = gr.Audio(label="输出",
+            audio = gr.Audio(label="Sortie",
                              interactive=False,
                              elem_classes="audio")
-            clear = gr.Button("清除", elem_id="clear-btn")
+            clear = gr.Button("Effacer", elem_id="clear-btn")
             btn.click(fn=textToSpeech,
                       inputs=[text, voices, rate, volume],
                       outputs=[audio])
